edc898341046d8b311cbeb76f3549de33a9d016d
Joy S <stepjoyous@gmail.com>
2024-02-27 22:33:58 -0800

    Updated Task component.

--- taskbox/src/components/Task.jsx
+++ taskbox/src/components/Task.jsx
@@ -6,10 +6,43 @@ const Task = ({
   onPinTask
 }) => {
   return (
-    <div className="list-item">
-      <label htmlFor="title" aria-label={title}>
-        <input type="text" value={title} readOnly={true} name="title"/>
+    <div className={`list-item ${state}`}>
+      <label
+        htmlFor="checked"
+        aria-label={`archiveTask-${id}`}
+        className="checkbox">
+        <input
+          type="checkbox"
+          disabled={true}
+          name="checked"
+          id={`archiveTask-${id}`}
+          checked={state === "TASK_ARCHIVED"}
+        />
+        <span
+          className="checkbox-custom"
+          onClick={() => onArchiveTask(id)}>
+        </span>
       </label>
+
+      <label htmlFor="title" aria-label={title} classname="title">
+        <input
+          type="text"
+          value={title}
+          readOnly={true}
+          name="title"
+          placeholder="Input title"
+        />
+      </label>
+
+      { state !== "TASK_ARCHIVED" && (
+        <button
+          className="pin-bottom"
+          onClick={() => onPinTask(id)}
+          id={`pinTask-${id}`}
+          key={`pinTask-${id}`}>
+          <span className={`icon-star`} />
+        </button>
+      )}
     </div>
   )
 }
